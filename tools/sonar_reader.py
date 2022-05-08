f01b289c3f9f0ae8f469934439df0980d7924d21
Stratos <stratoscaster191@gmail.com>
2022-05-07 17:21:56 -0700

    Enabled debug mode

--- tools/sonar_reader.py
+++ tools/sonar_reader.py
@@ -6,7 +6,7 @@ class SonarReader:
 
     def __init__(self):
         # Sonar reader cables from LEFT to RIGHT (in the module itself):purple, blue, green, yellow
-        self.DEBUG_MODE = False
+        self.DEBUG_MODE = True
         IO.setmode(IO.BOARD)
         self.EMPTY_DISTANCE = 6.25
         self.FULL_DISTANCE = 1
